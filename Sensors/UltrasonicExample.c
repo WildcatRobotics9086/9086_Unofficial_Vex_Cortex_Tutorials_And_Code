f8ea6ae60cbba04d944e143f5e775e2a17775aff
WackyTubeman <99372113+WackyTubeman@users.noreply.github.com>
2023-08-07 13:30:13 -0400

    Create UltrasonicExample.c

---
+++ Sensors/UltrasonicExample.c
@@ -0,0 +1,9 @@
+/*
+This snippet is designed to display how to make the component work and show all of the functions for them.
+
+Display: 
+  Sensor output
+Notes:
+  Debug console also shows this data
+*/
+
