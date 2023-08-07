16d20511ec5ff976fb708b43e2ae472b5b28dbac
WackyTubeman <99372113+WackyTubeman@users.noreply.github.com>
2023-08-07 12:59:01 -0400

    Update and rename ServoMotorExample to ServoMotorExample.c

---
+++ Motors/ServoMotorExample.c
@@ -0,0 +1,8 @@
+/*
+This snippet is designed to display how to make the component work and show all of the functions for them.
+
+Display: 
+  Servo on port 2 moves to 127 and waits for 10 seconds, then goes to -127 and waits for 10 seconds, and finally returns to 0.
+Notes:
+  No motor controller needed between motor and Cortex
+*/
