ed55af1bde354cd5e767f45eb76355451e720f2e
WackyTubeman <99372113+WackyTubeman@users.noreply.github.com>
2023-08-07 13:39:40 -0400

    Create ShaftEncoder.c

---
+++ Sensors/ShaftEncoder.c
@@ -0,0 +1,19 @@
+/*
+This snippet is designed to display how to make the component work and show all of the functions for them.
+
+Display: 
+  Sensor gathers data then outputs data and waits 1 second
+Notes:
+  Debug console also shows this data
+*/
+
+//Use ROBOTC Configuration Wizard (Motor and Sensor button) to set up ports
+task main(){
+  //Use a while() statement to constantly repeat the process
+  while(true) {
+    //Reads the sensor and outputs the data to the debug stream (debug stream may not open automatically you may have to open it manually using the menus at the top)
+    encoder = SensorValue(dgtl2);
+		writeDebugStreamLine("Rotations:", encoder);
+    wait(1000);
+  }
+}
