f7dc32c249f28202c60b9b9ea50285aac3cef0c5
WackyTubeman <99372113+WackyTubeman@users.noreply.github.com>
2023-10-26 09:29:44 -0400

    Update if_statements.c

--- RobotC/if_statements.c
+++ RobotC/if_statements.c
@@ -5,24 +5,32 @@
 int variable1 = 1; //sets an integer named variable1 to 1 for demonstration (and might not work for all of the statements listed below indicated by "*")
 int varaible2 = 2; //sets an integer named variable2 to 2 for demonstration (and might not work for all of the statements listed below indicated by "*")
 
-if (variable1 == 1) { //runs when variable is equal to 1
+if (variable1 == 1) { //runs when variable1 is equal to 1
   //your code here
 }
-if (variable1 <= 1) { // * runs when variable is less than or equal to 1
+if (variable1 <= 1) { // * runs when variable1 is less than or equal to 1
   //your code here
 }
-if (variable1 < 1) { // * runs when variable is less than 1
+if (variable1 < 1) { // * runs when variable1 is less than 1
   //your code here
 }
-if (variable1 >= 1) { // * runs when variable is greater than or equal to 1
+if (variable1 >= 1) { // * runs when variable1 is greater than or equal to 1
   //your code here
 }
-if (variable1 > 1) { // * runs when variable is greater than 1
+if (variable1 > 1) { // * runs when variable1 is greater than 1
   //your code here
 }
-if (variable1 != 1) { // * runs when variable is not equal to 1
+if (variable1 != 1) { // * runs when variable1 is not equal to 1
   //your code here
 }
-if ((variable1 == 1) && (variable2 == 2)) { //runs when variable is equal to 1
+if ((variable1 == 1) && (variable2 == 2)) { //runs when variable1 is equal to 1 and variable2 is 2
   //your code here
 }
+
+if (variable1 == 1) { //runs when variable1 is equal to 1
+  //your code here
+}
+else { //runs when the previous condition is not met
+  //more code here
+}
+
