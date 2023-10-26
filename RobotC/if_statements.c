49fa68f21b27124c5fd4da1f5898a26f24fee979
WackyTubeman <99372113+WackyTubeman@users.noreply.github.com>
2023-10-26 09:25:14 -0400

    Create if_statements.c

---
+++ RobotC/if_statements.c
@@ -0,0 +1,28 @@
+//C^2
+//The code presented here is designed to demonstrate how to use an "if" statement for basic logic.
+//This code will not work as intended and is designed to show only how to implement "if" statements
+
+int variable1 = 1; //sets an integer named variable1 to 1 for demonstration (and might not work for all of the statements listed below indicated by "*")
+int varaible2 = 2; //sets an integer named variable2 to 2 for demonstration (and might not work for all of the statements listed below indicated by "*")
+
+if (variable1 == 1) { //runs when variable is equal to 1
+  //your code here
+}
+if (variable1 <= 1) { // * runs when variable is less than or equal to 1
+  //your code here
+}
+if (variable1 < 1) { // * runs when variable is less than 1
+  //your code here
+}
+if (variable1 >= 1) { // * runs when variable is greater than or equal to 1
+  //your code here
+}
+if (variable1 > 1) { // * runs when variable is greater than 1
+  //your code here
+}
+if (variable1 != 1) { // * runs when variable is not equal to 1
+  //your code here
+}
+if ((variable1 == 1) && (variable2 == 2)) { //runs when variable is equal to 1
+  //your code here
+}
